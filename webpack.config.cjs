2a20ea033e6b38cb9271d4c45249abb5ae09668b
Kuldeepthedev <124391385+Kuldeepthedev@users.noreply.github.com>
2024-08-16 17:29:46 +0530

    final

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   mode: 'production',
   entry: './index.js',
@@ -7,7 +9,6 @@ module.exports = {
     library: 'reactApiHandling',
     libraryTarget: 'umd',
     globalObject: 'this',
-    umdNamedDefine: true // Optional: names your module when using AMD
   },
   devtool: 'source-map',
   module: {
@@ -18,16 +19,16 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
-      }
-    ]
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
   },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx'],
   },
   externals: {
-    react: 'react' // Exclude React from the bundle
-  }
+    react: 'react',
+  },
 };
